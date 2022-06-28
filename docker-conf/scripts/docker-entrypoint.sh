3c2d964624526f6172f2d6be3a5bc19716f7600a
trillo <sk8unk@gmail.com>
2022-06-28 13:13:20 -0400

    no sphinxsearch

--- docker-conf/scripts/docker-entrypoint.sh
+++ docker-conf/scripts/docker-entrypoint.sh
@@ -4,10 +4,6 @@
 /etc/init.d/nginx restart
 /etc/init.d/php8.1-fpm restart
 
-# START sphinx
-indexer --all --rotate
-searchd
-
 # START octane swoole
 /bin/php /app/artisan config:cache
 /bin/php /app/artisan route:cache
--- docker-conf/scripts/docker-entrypoint_dev.sh
+++ docker-conf/scripts/docker-entrypoint_dev.sh
@@ -4,9 +4,5 @@
 /etc/init.d/nginx restart
 /etc/init.d/php8.1-fpm restart
 
-# START sphinx
-indexer --all --rotate
-searchd
-
 # KEEP CONTAINER ALIVE
 tail -f /dev/null
