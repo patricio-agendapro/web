c66a23bbbbbb4ea61401676e173b177a9d443bc5
trillo <sk8unk@gmail.com>
2022-07-06 12:10:14 -0400

    strip top

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -61,17 +61,18 @@
 <body class="home">
   <div class="content-wrapper">
     <header class="wrapper">
-      <div class="bg-primary text-white fw-bold fs-15 mb-2 d-none d-lg-block">
+      <div class="bg-soft-yellow text-leaf fs-13 mb-2 d-none d-lg-block">
         <div class="container py-2 d-md-flex flex-md-row">
           <div class="d-flex flex-row align-items-center">
+            <span>Desde nuestro Blog: <a class="link-leaf hover-3 more" href="#">Pérdida de clientes: Causas y cómo evitarlas</a></span>
           </div>
           <div class="d-flex flex-row align-items-center me-6 ms-auto">
-            <div class="icon text-white fs-22 mt-1 me-2"> <i class="uil uil-phone-volume"></i></div>
+            <div class="icon text-leaf fs-18 mt-0 me-1"> <i class="uil uil-phone-volume"></i></div>
             <p class="mb-0">+562 2938 1145</p>
           </div>
           <div class="d-flex flex-row align-items-center">
-            <div class="icon text-white fs-22 mt-1 me-2"> <i class="uil uil-message"></i></div>
-            <p class="mb-0"><a href="mailto:sandbox@email.com" class="link-white hover">contacto@agendapro.com</a></p>
+            <div class="icon text-leaf fs-18 mt-0 me-1"> <i class="uil uil-message"></i></div>
+            <p class="mb-0"><a href="mailto:sandbox@email.com" class="link-leaf hover">contacto@agendapro.com</a></p>
           </div>
         </div>
         <!-- /.container -->
