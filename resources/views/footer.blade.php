b26381867f4736983917f9fec573e81f25ea3681
trillo <sk8unk@gmail.com>
2022-08-09 11:15:04 -0400

    footer img height

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -5,7 +5,7 @@
       <div class="row mt-15 mb-8">
         <div class="col-md-4 col-lg-4">
           <div class="widget">
-            <img class="mb-4" width="240" src="/assets/img/logoapro.svg" alt="AgendaPro" />
+            <img class="mb-4" width="240" class="lazyload" src="/assets/img/logoapro.svg" alt="AgendaPro" />
             <nav class="nav social ">
               
               <span class="avatar bg-pale-primary text-primary w-9 h-9"><a href="#"><i class="uil text-green uil-instagram"></i></a></span>
